26efd4f11f52a05db360f52604ccf4a8fa5f939d
Kasun Prabashwara <kasunprabashwara24@gmail.com>
2023-01-07 11:33:55 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+  const [review, setReview] = useState("");
+  const handleSubmit = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      cusName: name,
+      review: review,
+    }).then(() => {
+      alert("success");
+    });
+  };
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      console.log(response.data);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
+      <div className="form">
+        <label>Name</label>
+        <input
+          type="text"
+          name="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Review</label>
+        <input
+          type="text"
+          name="Review"
+          onChange={(e) => setReview(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
 }
