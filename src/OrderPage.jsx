2bf0811b9cbc3a5d20554232787eb0880f04b05d
Kasun Prabashwara <kasunprabashwara24@gmail.com>
2023-01-10 17:00:49 +0530

    done for the most part

--- src/OrderPage.jsx
+++ src/OrderPage.jsx
@@ -1,34 +1,40 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function OrderPage() {
-  const items = [
-    { id: 1, name: "Bottle", price: 10 },
-    { id: 2, name: "Pen", price: 5 },
-    { id: 3, name: "Pencil", price: 2 },
-  ];
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/products").then((res) => {
+      setItems(res.data);
+    });
+  }, []);
+
   const [cart, setCart] = useState([]);
-  const updateCart = (id, value) => {
-    id = parseInt(id);
+  const updateCart = (product, value) => {
+    const id = parseInt(product.Product_ID);
     const newCart = [];
     let isfound = false;
     for (const item of cart) {
-      if (item.id == id) {
+      if (item.Product_ID == id) {
         item.quantity = value;
         isfound = true;
-        if (value == 0) {
+        if (parseInt(value) <= 0) {
           continue;
         }
       }
       newCart.push(item);
     }
     if (!isfound) {
-      newCart.push({ id: id, quantity: value });
+      newCart.push({ ...product, quantity: value });
     }
-    console.log(newCart);
     setCart(newCart);
   };
   const placeOrder = () => {
-    console.log("Order Placed");
+    axios.post("http://localhost:3001/placeOrder", cart).then((res) => {
+      if (res) {
+        alert("Order Placed Successfully");
+      }
+    });
   };
 
   return (
@@ -44,15 +50,16 @@ function OrderPage() {
         <tbody>
           {items.map((item, key) => {
             return (
-              <tr>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
+              <tr key={item.Product_ID}>
+                <td>{item.Product_ID}</td>
+                <td>{item.Product_Name}</td>
+                <td>{item.Price}</td>
+                <td>{item.Capasity_consumption}</td>
                 <td>
                   <input
                     type="number"
                     className="form-control"
-                    onChange={(e) => updateCart(item.id, e.target.value)}
+                    onChange={(e) => updateCart(item, e.target.value)}
                   />
                 </td>
               </tr>
@@ -65,13 +72,15 @@ function OrderPage() {
         <table className="table table-hover">
           <thead className="thead light">
             <th>ID</th>
+            <th>Product Name</th>
             <th>Qantity</th>
           </thead>
           <tbody>
             {cart.map((item, key) => {
               return (
-                <tr>
-                  <td>{item.id}</td>
+                <tr key={item.Product_ID}>
+                  <td>{item.Product_ID}</td>
+                  <td>{item.Product_Name}</td>
                   <td>{item.quantity}</td>
                 </tr>
               );
